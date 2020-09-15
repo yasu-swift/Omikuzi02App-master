99e00d0ed33b770b90df24d9197ab07d5976136d
yasu-swift <sp4c72q9@icloud.com>
2020-09-15 20:14:08 +0900

    紐付け

---
+++ OmikuziApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  OmikuziApp
+//
+//  Created by 高橋康之 on 2020/09/15.
+//  Copyright © 2020 yasu.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBOutlet var StickView: UIView!
+    
+    
+    @IBOutlet var StickLabel: UILabel!
+    
+    @IBOutlet var stickHeight: NSLayoutConstraint!
+    
+    @IBOutlet var stickBottomMargin: NSLayoutConstraint!
+    
+}
+
